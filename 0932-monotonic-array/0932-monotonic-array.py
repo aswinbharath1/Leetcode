870c75f57807d406964a2a813c667a2b2f917a85
Aswin Bharath <60819581+aswinbharath1@users.noreply.github.com>
2023-09-29 12:05:03 +0530

    Time: 839 ms (52.88%), Space: 30.4 MB (95.58%) - LeetHub

---
+++ 0932-monotonic-array/0932-monotonic-array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        if len(nums) < 2:
+            return True
+        
+        direction = 0 
+        
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i-1]:
+                if direction == 0:
+                    direction = 1
+                elif direction == -1:
+                    return False
+            elif nums[i] < nums[i-1]:  
+                if direction == 0:
+                    direction = -1
+                elif direction == 1:
+                    return False
+        
+        return True
