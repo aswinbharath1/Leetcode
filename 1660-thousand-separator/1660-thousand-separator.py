75b3eb961101b4400f3be8572593463bf7163ffc
Aswin Bharath <60819581+aswinbharath1@users.noreply.github.com>
2023-10-10 10:43:13 +0530

    Time: 46 ms (5.07%), Space: 16.3 MB (50.68%) - LeetHub

--- 1660-thousand-separator/1660-thousand-separator.py
+++ 1660-thousand-separator/1660-thousand-separator.py
@@ -1,22 +1,3 @@
 class Solution:
     def thousandSeparator(self, n: int) -> str:
-        if n<1000:
-            return str(n)
-        else:
-            t=n
-            n=list(str(n))
-            if t<999999 : 
-                n.insert(-3,'.')
-                n=''.join(n)
-                return str(n)
-            elif t<999999999: 
-                n.insert(-3,'.')           
-                n.insert(-7,'.')
-                n=''.join(n)
-                return str(n)
-            else:
-                n.insert(-3,'.')           
-                n.insert(-7,'.')
-                n.insert(-11,'.')
-                n=''.join(n)
-                return str(n)
+        return f"{n:,}".replace(",",".")
