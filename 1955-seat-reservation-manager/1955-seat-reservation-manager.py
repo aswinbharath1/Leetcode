aa7ca6f5d4f8ecd9b6f1b33eb09a9a310bc5b065
Aswin Bharath <60819581+aswinbharath1@users.noreply.github.com>
2023-11-06 11:20:56 +0530

    Time: 449 ms (51.07%), Space: 43.9 MB (67.29%) - LeetHub

---
+++ 1955-seat-reservation-manager/1955-seat-reservation-manager.py
@@ -0,0 +1,18 @@
+class SeatManager:
+
+    def __init__(self, n: int):
+        self.heap=list(range(1,n+1))
+
+    def reserve(self) -> int:
+        return heapq.heappop(self.heap)
+
+    def unreserve(self, seatNumber: int) -> None:
+        heapq.heappush(self.heap,seatNumber)
+
+        
+
+
+# Your SeatManager object will be instantiated and called as such:
+# obj = SeatManager(n)
+# param_1 = obj.reserve()
+# obj.unreserve(seatNumber)
